cd69b232a9afa2e610ae934c78b62e5d9597eb72
supremestdoggo <83146042+supremestdoggo@users.noreply.github.com>
2021-12-28 12:42:43 -0500

    Create tmt.gd

---
+++ tmt.gd
@@ -0,0 +1,13 @@
+const BUF_MAX = 100
+const PAR_MAX = 8
+const TAB = 8
+func MAX(x, y):
+  if x > y:
+    return x
+  else:
+    return y
+func MIN(x, y):
+  if x < y:
+    return x
+  else:
+    return y
