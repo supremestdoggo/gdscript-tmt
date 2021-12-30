29935cd2f9fe4cc2b23ca30ebc4f9442ce82d571
supremestdoggo <83146042+supremestdoggo@users.noreply.github.com>
2021-12-30 11:23:08 -0500

    Update tmt.gd

--- tmt.gd
+++ tmt.gd
@@ -1,13 +1,47 @@
+"""
+Copyright (c) 2021 supremestdoggo
+Copyright (c) 2017 Rob King
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+# While constants and functions are perferred, macros may be used when needed via the AnyMacro software. See the README section on macros and building this repo for more info
 const BUF_MAX = 100
 const PAR_MAX = 8
 const TAB = 8
 func MAX(x, y):
-  if x > y:
-    return x
-  else:
-    return y
+  return x if x > y else y
 func MIN(x, y):
-  if x < y:
-    return x
-  else:
-    return y
+  return x if x < y else y
+func CLINE(vt):
+  return vt.screen.lines[MIN(vt.curs.r, vt.screen.nline - 1)]
+
+#define P0(x) (vt.pars[x])
+#define P1(x) (vt.pars[x] if vt.pars[x] else 1)
+func CB(vt, m, a):
+  return vt.cb(m, vt, a, vt.p) if vt.cb else null
+#define INESC (vt.state)
